ec9f4b6a1dfa19b34fcd206ae9c904c6ed418fb9
Andrej Gasic <andrej@gasic.rs>
2021-01-04 22:12:21 +0100

    Dodavanje messagebrokera u Eureka servis

---
+++ src/main/java/com/bosanskilonac/es/MessageBroker.java
@@ -0,0 +1,13 @@
+package com.bosanskilonac.es;
+
+import org.apache.activemq.broker.BrokerService;
+
+public class MessageBroker {
+
+	public static void main(String[] args) throws Exception {
+		BrokerService broker = new BrokerService();
+		broker.addConnector("tcp://localhost:61616");
+		broker.start();
+	}
+
+}
