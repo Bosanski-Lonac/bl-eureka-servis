ff4526c08e4a408cb122d74cb6a5b07797e0ebd3
Andrej Gasic <andrej@gasic.rs>
2021-01-04 22:39:14 +0100

    Popravljanje messagebrokera

--- src/main/java/com/bosanskilonac/es/EurekaServiceApplication.java
+++ src/main/java/com/bosanskilonac/es/EurekaServiceApplication.java
@@ -1,5 +1,6 @@
 package com.bosanskilonac.es;
 
+import org.apache.activemq.broker.BrokerService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
@@ -10,6 +11,16 @@ public class EurekaServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaServiceApplication.class, args);
+		
+		BrokerService broker = new BrokerService();
+		//broker.setUseShutdownHook(false);
+		try {
+			broker.addConnector("tcp://localhost:61616");
+			broker.start();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
